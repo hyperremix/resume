3dc11d41334cbd14c7b24eee481777a990f3496a
Fredrik Pettersson <fredrik.pettersson.89@gmail.com>
2024-10-26 15:45:22 +0200

    Add possibility to zoom profile picture

--- src/app/components/ProfilePicture/index.tsx
+++ src/app/components/ProfilePicture/index.tsx
@@ -1,21 +1,50 @@
+import { Modal } from '@mui/material';
 import { IKImage } from 'imagekitio-react';
-import React from 'react';
+import React, { useState } from 'react';
 
 type Props = {
   size?: number;
 };
 
-export const ProfilePicture = ({ size = 240 }: Props) => (
-  <IKImage
-    path="/profile-primary"
-    transformation={[
-      {
-        width: `${size}`,
-        height: `${size}`,
-        focus: 'auto',
-      },
-    ]}
-    lqip={{ active: true }}
-    style={{ borderRadius: '50%', width: `${size / 2}px`, height: `${size / 2}px` }}
-  />
-);
+export const ProfilePicture = ({ size = 240 }: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <IKImage
+        path="/profile-primary"
+        transformation={[
+          {
+            width: `${size}`,
+            height: `${size}`,
+            focus: 'auto',
+          },
+        ]}
+        lqip={{ active: true }}
+        style={{ borderRadius: '50%', width: `${size / 2}px`, height: `${size / 2}px` }}
+        onClick={() => setIsModalOpen(true)}
+      />
+      <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <IKImage
+          path="/profile-primary"
+          transformation={[
+            {
+              width: `768`,
+              height: `768`,
+              focus: 'auto',
+            },
+          ]}
+          lqip={{ active: true }}
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: `${768 / 2}px`,
+            height: `${768 / 2}px`,
+          }}
+        />
+      </Modal>
+    </>
+  );
+};
